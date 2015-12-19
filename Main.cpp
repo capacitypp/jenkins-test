1301a323dc78e67708eb8e63eba66affa4c9c284
capacitypp <aoi0x4c4f5645@gmail.com>
2015-12-19 13:09:01 +0900

    Eigen版hello, world!
    MakefileにEigenのインクルードパスを追加
    Main.cppをEigen版に変更

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+#include <Eigen/Core>
+
 using namespace std;
+using namespace Eigen;
 
 int main(int argc, char** argv)
 {
 	cout << "hello, world" << endl;
+
+	MatrixXd m1 = MatrixXd::Identity(3, 3);
+	MatrixXd m2 = 2 * m1;
+
+	cout << "[m1]" << endl;
+	cout << m1 << endl;
+
+	cout << "[m2 = 2 * m1]" << endl;
+	cout << m2 << endl;
 
 	return 0;
 }
--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 OBJS=Main.o
+INCLUDE=-I./include -I./
+CPPFLAGS=$(INCLUDE)
 
 all : $(OBJS)
 	$(CXX) -o Main $(LDFLAGS) $(OBJS)
