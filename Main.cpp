831be37d096698c7bdb776af29b80f6263f4c783
capacitypp <aoi0x4c4f5645@gmail.com>
2015-12-24 10:21:37 +0900

    変更 : ファイルパスを引数として受け取る

--- Main.cpp
+++ Main.cpp
@@ -9,6 +9,16 @@ using namespace Eigen;
 
 int main(int argc, char** argv)
 {
+	if (argc != 3) {
+		cerr << argv[0] << " <file path 1> <file path 2>" << endl;
+		return 1;
+	}
+
+	string filePath1(argv[1]);
+	string filePath2(argv[2]);
+
+	cout << filePath1 << endl;
+	cout << filePath2 << endl;
 
 	return 0;
 }
