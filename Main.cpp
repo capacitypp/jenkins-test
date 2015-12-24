f3deb5a7530731fb298dcbc73b39ca80191f785e
capacitypp <aoi0x4c4f5645@gmail.com>
2015-12-24 10:14:10 +0900

    削除 : テストコード

--- Main.cpp
+++ Main.cpp
@@ -9,23 +9,6 @@ using namespace Eigen;
 
 int main(int argc, char** argv)
 {
-	cout << "hello, world" << endl;
-
-	MatrixXd m1 = MatrixXd::Identity(3, 3);
-	MatrixXd m2 = 2 * m1;
-
-	cout << "[m1]" << endl;
-	cout << m1 << endl;
-
-	cout << "[m2 = 2 * m1]" << endl;
-	cout << m2 << endl;
-
-	Mat image = imread("data/lena.jpg");
-
-	namedWindow("window");
-	imshow("window", image);
-
-	cvWaitKey();
 
 	return 0;
 }
