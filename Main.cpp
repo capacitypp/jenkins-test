03d1654a43b86865b97922271db2e807e27225d0
capacitypp <aoi0x4c4f5645@gmail.com>
2015-12-26 16:49:07 +0900

    変更 : 対応点座標を引数として受け取る

--- Main.cpp
+++ Main.cpp
@@ -11,16 +11,16 @@ using namespace Eigen;
 
 int main(int argc, char** argv)
 {
-	if (argc != 3) {
-		cerr << argv[0] << " <file path 1> <file path 2>" << endl;
+	if (argc != 5) {
+		cerr << argv[0] << " <image path 1> <image path 2> <positions path 1> <positions path 2>" << endl;
 		return 1;
 	}
 
-	string filePath1(argv[1]);
-	string filePath2(argv[2]);
+	string imagePath1(argv[1]);
+	string imagePath2(argv[2]);
 
-	Mat image1 = imread(filePath1);
-	Mat image2 = imread(filePath2);
+	Mat image1 = imread(imagePath1);
+	Mat image2 = imread(imagePath2);
 
 	Mat gray_image1, gray_image2;
 	cvtColor(image1, gray_image1, CV_RGB2GRAY);
@@ -28,6 +28,9 @@ int main(int argc, char** argv)
 
 	MatrixXd gray1 = MatrixUtil::convertGray2MatrixXd(gray_image1);
 	MatrixXd gray2 = MatrixUtil::convertGray2MatrixXd(gray_image2);
+
+	string positionsPath1(argv[3]);
+	string positionsPath2(argv[4]);
 
 	return 0;
 }
